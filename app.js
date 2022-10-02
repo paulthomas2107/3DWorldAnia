f33e94a0810212594d8431b8cf34155a15eb5056
Paul Thomas <paulthomas2107@gmail.com>
2022-10-02 11:45:49 +0100

    More....

---
+++ app.js
@@ -0,0 +1,39 @@
+function main() {
+  // camera renderer
+  const canvas = document.querySelector('#c');
+
+  const fov = 75;
+  // const aspect = 2
+  const aspect = canvas.clientWidth / canvas.clientHeight;
+  const near = 0.1;
+  const far = 2000;
+
+  const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+  camera.position.z = 1;
+
+  const renderer = new THREE.WebGLRenderer({ canvas });
+  const width = canvas.clientWidth;
+  const height = canvas.clientHeight;
+
+  renderer.setSize(width, height);
+  new THREE.OrbitControls(camera, canvas);
+
+  const scene = new THREE.Scene();
+  const loader = new THREE.TextureLoader();
+  const texture = loader.load(
+    'https://threejs.org/manual/examples/resources/images/equirectangularmaps/tears_of_steel_bridge_2k.jpg',
+    () => {
+      const rt = new THREE.WebGLCubeRenderTarget(texture.image.height);
+      rt.fromEquirectangularTexture(renderer, texture);
+      scene.background = rt.texture;
+    }
+  );
+
+  function render() {
+    renderer.render(scene, camera);
+    requestAnimationFrame(render);
+  }
+  requestAnimationFrame(render);
+}
+
+main();
